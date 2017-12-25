e10aa16adc05f139496b9aee81de0d00559b4589
Alexandr <podhvatovshurik@gmail.com>
2017-12-25 17:57:56 +0200

    without back

--- quantal/js/main.js
+++ quantal/js/main.js
@@ -205,27 +205,4 @@ function scrollToAbout() {
 	$("html, body").animate({ scrollTop: height }, 600);
 };
 
-
-
-/*carousel*/
-$(document).ready(function(){
-	$('.owl-carousel').owlCarousel({
-	loop:true,
-	margin:50,
-	nav:true,
-	navText:['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
-	responsive:{
-	    0:{
-	        items:1
-	    },
-	    600:{
-	        items:2
-	    },
-	    1000:{
-	        items:3
-	    }
-	}
-	});
-});
-
 /* Endof scrolling functions */
